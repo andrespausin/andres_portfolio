25dbe9f6da8a2c376892c20bdf3088cd61a783b1
Andres Pausin <candrespausin2001@gmail.com>
2026-01-10 01:04:38 +0100

    add: created the project type of data

---
+++ lib/projects.ts
@@ -0,0 +1,13 @@
+export const projects = [
+  {
+    slug: "bob-abre-baseball-academy",
+    title: "Proyecto en Bob Abreu Baseball Academy",
+    description: "Desarrollo de una plataforma web para la gestión de entrenamientos y eventos deportivos.",
+    stack: [
+      "React",
+      "Node.js",
+      "Express",
+      "MySQL",
+    ]
+  },
+]
