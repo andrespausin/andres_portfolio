4022f93548874960795857b0e5d77e5d491aa4ef
Andres Pausin <candrespausin2001@gmail.com>
2026-01-13 10:47:15 +0100

    add: array of skills and logic for the UI compnents

---
+++ lib/skills.ts
@@ -0,0 +1,84 @@
+export interface Skill {
+  name: string
+  icon?: string
+}
+
+export interface Stack {
+  id: string
+  categoria: string
+  descripcion?: string
+  habilidades: (Skill | string)[]
+}
+
+export const techStack = [
+  {
+    id: "lenguajes-programacion",
+    categoria: "Lenguajes de Programación",
+    descripcion: "Core técnico y fundamentos sólidos",
+    habilidades: [
+      { name: "JavaScript", icon: "devicon-javascript-plain colored" },
+      { name: "TypeScript", icon: "devicon-typescript-plain colored" },
+      { name: "Python", icon: "devicon-python-plain colored" },
+      { name: "Java", icon: "devicon-java-plain colored" },
+      { name: "HTML5", icon: "devicon-html5-plain colored" },
+      { name: "CSS3", icon: "devicon-css3-plain colored" }
+    ]
+  },
+  {
+    id: "desarrollo-frontend",
+    categoria: "Desarrollo Frontend",
+    descripcion: "Interfaces interactivas y reactivas",
+    habilidades: [
+      { name: "React.js", icon: "devicon-react-original colored" },
+      { name: "Next.js", icon: "devicon-nextjs-plain" },
+      { name: "Angular", icon: "devicon-angularjs-plain colored" },
+      { name: "React Native", icon: "devicon-react-original colored" }
+    ]
+  },
+  {
+    id: "desarrollo-backend",
+    categoria: "Backend y Base de Datos",
+    descripcion: "Lógica de servidor y persistencia",
+    habilidades: [
+      { name: "Node.js", icon: "devicon-nodejs-plain colored" },
+      { name: "Express.js", icon: "devicon-express-original" },
+      { name: "NestJS", icon: "devicon-nestjs-plain colored" },
+      { name: "SQL", icon: "devicon-postgresql-plain colored" },
+      { name: "NoSQL", icon: "devicon-mongodb-plain colored" },
+    ]
+  },
+  {
+    id: "gestion-estado",
+    categoria: "Gestión de Estado",
+    descripcion: "Manejo de flujo de datos complejo",
+    habilidades: [
+      { name: "Redux Toolkit", icon: "devicon-redux-original colored" },
+      { name: "Context API", icon: "devicon-react-original colored" }
+    ]
+  },
+  {
+    id: "devops-herramientas",
+    categoria: "DevOps, Testing y Otras Herramientas",
+    descripcion: "Automatización y diagnóstico",
+    habilidades: [
+      { name: "Git", icon: "devicon-git-plain colored" },
+      { name: "Pipelines CI/CD", icon: "devicon-githubactions-plain colored" },
+      { name: "Docker", icon: "devicon-docker-plain colored" },
+      { name: "Jest", icon: "devicon-jest-plain colored" },
+      { name: "Vitest", icon: "devicon-vitest-plain colored" },
+      { name: "Postman", icon: "devicon-postman-plain colored" },
+
+    ]
+  },
+  {
+    id: "habilidades-blandas",
+    categoria: "Habilidades blandas",
+    habilidades: [
+      "Comunicación Efectiva",
+      "Resolución de Problemas",
+      "Autonomía y Proactividad",
+      "Gestión del Trabajo bajo Presión",
+      "Responsabilidad Profesional"
+    ]
+  }
+];
