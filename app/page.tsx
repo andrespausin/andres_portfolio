949066bc5178c2f3cad835dbfd4371fc3bbb188e
Andres Pausin <candrespausin2001@gmail.com>
2026-01-13 10:45:53 +0100

    edit: modified the page.tsx section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import { projects } from "@/lib/projects";
 import Header from '@/components/layout/Header';
 import Hero from "@/components/layout/Hero";
+import StackSection from "@/components/layout/StackSection";
+StackSection
 
 export default function Home() {
   return (
     <div className="">
       <Header />
       <Hero />
-      <Hero />
-      <Hero />
+      <StackSection />
     </div>
   );
 }
