b6cbaf427719253653ed4d692b5093e79715c7a5
Andres Pausin <candrespausin2001@gmail.com>
2026-01-14 20:33:06 +0100

    edit: Modified the NavItem component

--- components/ui/NavItem.tsx
+++ components/ui/NavItem.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 
 const NavItem = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-black px-4 py-2 rounded-3xl hover:bg-white hover:text-black hover:rounded-3xl hover:transition-colors delay-150 duration-300 ease-in-out">
+    <div className="bg-black/30 backdrop-blur-md px-4 py-2 rounded-3xl hover:bg-white hover:text-black hover:rounded-3xl hover:transition-colors delay-150 duration-300 ease-in-out">
       {children}
     </div>
   )
