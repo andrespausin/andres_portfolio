5c72d1fec2a053453a013aac1f32cc7f5add0b6d
Andres Pausin <candrespausin2001@gmail.com>
2026-01-14 20:34:23 +0100

    edit: Modified the style of the Subheading component

--- components/ui/Subheading.tsx
+++ components/ui/Subheading.tsx
@@ -4,8 +4,23 @@ interface Props {
 
 export default function Subheading({ content }: Props) {
   return (
-    <p className="font-syne text-white tracking-tighter mt-3">
-      <span className="pl-2 block text-[clamp(1.5rem,8vw,3.5rem)] md:text-[clamp(3rem,14vw,3.5rem)] lg:text-[clamp(3rem,14vw,4rem)] xl:text-[clamp(3rem,14vw,4.5rem)] leading-[0.85] tracking-[-0.02em] font-serif text-stroke font-normal">
+    <p className="mt-3 overflow-hidden">
+      <span className="
+        block 
+        font-primary font-extrabold uppercase 
+        transition-all duration-500
+        
+        text-[clamp(1.5rem,8vw,3rem)] 
+        md:text-[clamp(3rem,14vw,3rem)] 
+        lg:text-[clamp(3rem,14vw,3.2rem)] 
+        xl:text-[clamp(3rem,14vw,4rem)] 
+        
+        leading-[0.85] 
+        pl-2
+
+        text-transparent 
+        [-webkit-text-stroke:2px_white] 
+      ">
         {content}
       </span>
     </p>
