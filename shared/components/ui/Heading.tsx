ddade377f3a3bc596de67b6f1ad8fc1894952e23
Andres Pasuin <candrespausin2001@gmail.com>
2026-01-23 15:35:36 +0100

    edit: Modified the width

--- shared/components/ui/Heading.tsx
+++ shared/components/ui/Heading.tsx
@@ -3,11 +3,9 @@ interface Props {
   className?: string
 }
 
-const textSize = "text-[clamp(3rem,14svw,6rem)] md:text-[clamp(3rem,14vw,8rem)] lg:text-[clamp(3rem,14vw,7.3rem)] xl:text-[clamp(3rem,14vw,7.5rem)]"
-
 export default function Heading({ content, className }: Props) {
   return (
-    <p className="font-inter font-semibold tracking-tighter w-full">
+    <p className="font-inter font-semibold tracking-tighter w-fit">
       <span className={`
       leading-[0.85] 
       tracking-[-0.04em] 
